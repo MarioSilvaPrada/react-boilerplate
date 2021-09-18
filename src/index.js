a2741b50980e07ba55c5d3e0a517e3fcf6d3ab98
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-09-18 23:03:26 +0100

    added regeneratorRuntime

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom';
-// import 'normalize.css';
+import {render} from 'react-dom';
+/*eslint-disable */
+import regeneratorRuntime from 'regenerator-runtime'; // to use async await
 import './index.css';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {ThemeProvider} from 'styled-components';
 import NavBar from './components/NavBar/NavBar';
 import App from './routes/App';
 import theme from './theme/theme';
@@ -21,10 +22,10 @@ const root = (
     <Router>
       <NavBar />
       <Switch>
-        <Route exact path='/' component={App} />
-        <Route exact path='/1' component={Page1} />
-        <Route exact path='/2' component={Page2} />
-        <Route path='*' component={PageNotFound} />
+        <Route exact path="/" component={App} />
+        <Route exact path="/1" component={Page1} />
+        <Route exact path="/2" component={Page2} />
+        <Route path="*" component={PageNotFound} />
       </Switch>
     </Router>
   </ThemeProvider>
